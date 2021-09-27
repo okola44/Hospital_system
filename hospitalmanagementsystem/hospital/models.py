bca99bdaac3bf5c65b9024fba7e7979c901cf31e
Okola44 <mercyimaliokola@gmail.com>
2021-09-27 21:20:42 +0300

    untracked files

--- hospitalmanagementsystem/hospital/models.py
+++ hospitalmanagementsystem/hospital/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    name=models.CharField(max_length=10,blank=True,null=True)
+    email=models.EmailField(unique=True,blank=True,null=True)
+    gender=models.CharField(max_length=20,blank=True,null=True)
+    phone_number=models.CharField(max_length=15,blank=True,null=True)
+    date_of_birth=models.DateField()
+    bloodgroup=models.CharField(max_length=5,blank=True,null=True)
+    address=models.CharField(max_length=20,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
--- hospitalmanagementsystem/hospital/views.py
+++ hospitalmanagementsystem/hospital/views.py
@@ -1,4 +1,7 @@
+from django.core import exceptions
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.models import User,Group
 
 # Create your views here.
 def homepage(request):
@@ -8,7 +11,39 @@ def aboutpage(request):
     return render (request,'about.html')
 
 def createaccountpage(request):
-    return render (request ,'create.html')
+    user="none"
+    error=""
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['password']
+        repeatpassword=request.POST['repeatpassword']
+        gender=request.POST['gender']
+        phonenumber=request.POST['phonenumber']
+        address=request.POST['address']
+        dateofbirth=request.POST['dateofbirth']
+        bloodgroup=request.POST['bloodgroup']
+    
+    
+        try:
+            if password==repeatpassword:
+                Patient.objects.create(name=name,email=email,gender=gender,phonenumber=phonenumber,address=address,dateofbirth=dateofbirth,bloodgroup=bloodgroup)
+                user=User.objects.create_user(first_name=name,email=email,password=password,username=email)
+                pat_group=Group.objects.get(name="patient")
+                pat_group.user_set.add(user)
+                user.save()
+                error="no"
+
+
+            else:
+                error="yes"
+        except Exception as e:
+
+            error="yes"
+
+    d={'error' : error}
+    return render (request ,'create.html',d)
+    
 
 def logInpage(request):
     return render (request ,'logIn.html')
